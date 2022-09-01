5d1d07a85570825f5395469a02e8bf61ae284d64
Dyoung <wkftkfwk0118@gmail.com>
2022-09-01 09:58:35 +0900

    노엘로힐스라우터및스토어생성

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+// 라우터를 연결합니다.
+// 파일의 확장자가 .js 라서 생략 했어요.
+import router from './router/index'
+import store from './store'
+createApp(App).use(router).use(store).mount('#app')
+// Vue 가 실행이 될때 제일 처음 불러서 실행하는 .js 파일입니다.
+// Entry Point 역할을 합니다.
+// Vue 어플리케이션에서 모두 참조가 됩니다.
+// router, vuex를 기본적으로 많이 추가합니다.
---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import {createRouter,createWebHistory} from "vue-router";
+import {createStore} from 'vuex';
+const router = createRouter({
+  // 웹브라우저 주소에서 #아이디 이동하는 흔적을
+  // 남기지 않는다.
+  history: createWebHistory(),
+  // 컴포넌트 연결 정보
+  // routes : [{path:'/글자', name: 'path의 별칭', component: 연결시켜줄 컴퍼넌트}]
+  // 예: routes : [ {path:'/good', name: 'Good', component:GoodView}]
+  // 가능하면, router 에 사용하는 컴포넌트는 src/view 폴더를 생성하고
+  // 여러 컴포넌트를 포함하는 vue 파일을 생성한다.
+  // 가능하면, 페이지이름 View. 라고 생성한다.
+  routes:[]
+  // routes: [{
+  //     path: '/',
+  //     name: 'Home',
+  //     redirect: '패스'
+  //   },
+  //   {
+  //     path: '/',
+  //     name: 'Home',
+  //     component: '컴포넌트명'
+  //   }
+  // ]
+});
+ //router 라는 변수를 다른 파일에서 사용하기 위해
+  //router 변수를 내보낼 겁니다.
+  // 여기서는 1개만 내보낼 겁니다.
+export default router;
